4c25f8cc194be69a872ce42e04b26fbf5c609d2f
Jaswanth Mariboina <469jaswanth@gmail.com>
2025-04-22 11:28:28 +0530

    Update SwaggerConfig.java

--- Hospital--Management/src/main/java/com/java/security/SwaggerConfig.java
+++ Hospital--Management/src/main/java/com/java/security/SwaggerConfig.java
@@ -13,7 +13,7 @@ public class SwaggerConfig {
     @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
-                .info(new Info().title("VIJAYA KUMARI-HOSPITALS")
+                .info(new Info().title("GRACE VALLEY-HOSPITAL")
                         .version("1.0")
                         .description("API documentation for Hospital Management System"))
                 .addSecurityItem(new SecurityRequirement().addList("BearerAuth"))
